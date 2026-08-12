90eddec8a539410ff55462e267e42b21bc12b566
Pavithra K <pavithrakannan010@gmail.com>
2026-08-12 21:40:32 +0530

    Connect frontend to live backend

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/projects")
+    fetch("http://https://portfolio-xoar.onrender.com/api/projects")
       .then((response) => response.json())
       .then((data) => setProjects(data))
       .catch((error) => console.error("Error fetching projects:", error));
